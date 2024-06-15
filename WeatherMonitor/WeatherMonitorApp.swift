9d4ceba0ac59add082474a5767d2e5ab6fb561ec
James Kowalski <jzk0156@auburn.edu>
2024-06-15 14:35:18 -0500

    Initial Commit

---
+++ WeatherMonitor/WeatherMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMonitorApp.swift
+//  WeatherMonitor
+//
+//  Created by James Kowalski on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
